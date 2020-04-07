568b6b91235e9075a9d622b590b3f0c40187b5a7
Antenor Manoel <antenor.filho@sankhya.com.br>
2020-04-07 00:32:53 -0300

    Adicionando classes JDBC e começo dos estilos.

---
+++ tradetask/src/main/java/models/MySqlConnector.java
@@ -0,0 +1,80 @@
+package models;                   
+ 
+//Classes necessárias para uso de Banco de dados
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConnector {
+
+	private static String status = "Não conectou...";
+	
+	//Método de Conexão
+	private static Connection getConexao(boolean isClose) {
+
+		Connection connection = null; //Variavel do tipo Connection
+
+		try {				
+			//Configurando a nossa conexão com um banco de dados//
+			final String serverName = "localhost"; //caminho do servidor do BD
+			final String mydatabase = "encomanager"; //nome do seu banco de dados
+			final String urlParams = "useTimezone=true&serverTimezone=UTC";
+			final String username = "root"; //nome de um usuário de seu BD
+			final String password = "admin"; //sua senha de acesso
+			String url = "jdbc:mysql://" + serverName + "/" + mydatabase + "?" + urlParams;
+					
+			connection = DriverManager.getConnection(url, username, password);
+			
+			//Seta status da conexão
+			if (connection != null) {
+				status = ("STATUS-> Conectado com sucesso!");
+			} else {
+				status = ("STATUS-> Não foi possivel realizar conexão");
+			}
+			return connection;
+
+		} catch (SQLException e) {
+			//Não conseguindo se conectar ao banco
+			System.out.println("Nao foi possivel conectar ao Banco de Dados.");
+			e.printStackTrace();
+			return null;
+			
+		} finally {
+			if (!isClose) {
+				System.out.println(status);
+			}	
+		}
+	}
+
+	//Método que abre sua conexão.
+	public static Connection abreConexao() {
+		return getConexao(false);
+	}
+	
+	//Método que fecha sua conexão
+	public static boolean fecharConexao() {	
+		try {
+			MySqlConnector.getConexao(true).close();
+			status = "STATUS-> Conexão finalizada!";
+			return true;
+		} catch (Exception e) {
+			System.out.println("Erro ao fechar conexão");
+			e.printStackTrace();
+			return false;
+		} finally {
+			System.out.println(status);
+		}
+	}
+	
+	//Método que reinicia sua conexão//
+	public static Connection reiniciarConexao() {
+		System.out.println("Reiniciando conexão");
+		fecharConexao();
+		return MySqlConnector.getConexao(false);
+	}
+	
+	//Método que retorna o status da sua conexão
+	public static void statusConexao() {
+		System.out.println(status);
+	}
+}
